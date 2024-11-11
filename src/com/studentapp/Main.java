fb2e20d26a75d6eea545d810093d98dd9feab66d
sktripathiinfo <sktripathiinfo@gmail.com>
2024-11-11 16:10:05 +0530

    removed unused classes

--- src/com/studentapp/Main.java
+++ src/com/studentapp/Main.java
@@ -1,42 +1,176 @@
 package com.studentapp;
 
 import java.nio.channels.NonReadableChannelException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.imageio.plugins.tiff.ExifGPSTagSet;
+import javax.management.RuntimeErrorException;
 
 public class Main {
+	
+	private static List<Student> studentList;
+	private static Scanner scanner;
 
 	public static void main(String[] args) {
 		
 		System.out.println("********** School Management System ********** ");
+		
+		
+		
+		studentList = new ArrayList<Student>();
+		scanner = new Scanner(System.in);
+		while(true) {
 		System.out.println("********** Welcome ********** ");
 		
-		Student s1 ;
-//		s1 = new Student(122,"shubham", "Java");        #invalid age =122
+		System.out.println(".....Select an Option.....");
+		System.out.println("1. Regester a Student");
+		System.out.println("2. Find Student with StudentID");
+		System.out.println("3. List all Student information");
+		System.out.println("4. List all Student information sorted order");
+		System.out.println("5. Exit");
+		int option = scanner.nextInt();
 		
-		//Valid age input
-		s1 = new Student(23,"shubham", "S-12"); 
+		switch (option) {
+		case 1:
+			enrollstudent(scanner);
+			
+			break;
+			
+		case 2: findStudentbyId(scanner);
 		
-		s1.enrollcource("java");
-		s1.enrollcource("devops");
-		s1.enrollcource("dsa");
-		s1.enrollcource("c#");
+		    break;
+		    
+		case 3: printAllstudentData();
 		
-//		System.out.println(s1);       
+		    break;
+		    
+		    
+		case 4: sortByName();
 		
-		s1.printStudentinfo();
-		
-		
-		Student s2 = new Student(25, "shubham pandey", "S-03");
-		s2.enrollcource("dsa");
-//		System.out.println(s2);
-		s2.printStudentinfo();
-		
-		Student s3 = new Student(25, "kajal", "S-02");
-		s2.enrollcource("devops");
-//		System.out.println(s3);
-		s3.printStudentinfo();
-		
+		   break;
+		   
+		case 5:Exit();
+			
 		
 
+		default: System.out.println("Invalid option Selected ! ...... Enter between 1 to 5 ");
+			break;
+			
+			
+		}
+		
+ }
+		
 	}
 
+	private static void Exit() {
+		System.out.println("Good Bye!!!");
+		System.exit(0);
+		
+	}
+
+	private static void sortByName() {
+		Comparator<Student>studentNameComparator = (o1,o2)-> o1.getName().compareTo(o2.getName());
+		Collections.sort(studentList,studentNameComparator);
+		System.out.println(studentList);
+	}
+
+	private static void printAllstudentData() {
+		if(studentList.size()>0) {
+		System.out.println("------------ Print All Information ------------");
+		for (Student student : studentList) {
+			student.printStudentinfo();
+		}
+		System.out.println("------------ ********** ------------");
+		}
+		else {
+			System.err.println("Student List is Empty!!! No Student Record found" );
+		}
+	}
+
+	private static void findStudentbyId(Scanner scanner2) {
+		Student studentFound = null;     //explicit initialization
+		
+		System.out.println("Enter the studentId");
+		String studentId = scanner2.next();
+		try {
+			studentFound= studentList
+		.stream().filter(student -> student.getStudentID()
+		.equalsIgnoreCase(studentId))
+        .findFirst()
+        .orElseThrow(()-> new RuntimeException("No Data found !!!"));
+		
+		}
+		catch (RuntimeException e) {
+			System.err.println("Student with ID "+studentId+"not found!!");
+		}
+		
+		studentFound.printStudentinfo();
+		
+	}
+
+	private static void enrollstudent(Scanner scanner2) {
+		System.out.println("Enter the Student Name");
+		String studentName = scanner2.next();
+		
+		System.out.println("Enter the Student age");
+		int studentage = scanner2.nextInt();
+		
+		System.out.println("Enter the Student StudentId");
+		String studentId = scanner2.next();
+		
+		Student newStudent = new Student(studentage, studentName, studentId);
+		studentList.add(newStudent);
+		
+		System.out.println("Enter the Course to be enrolled!!l...");
+		String courseName = scanner2.next();
+		while(true) {
+			System.out.println("Enter the Course to be enrolled!!l... Type Done to exit");
+			String courseName1 = scanner2.next();
+			if(courseName1.equalsIgnoreCase("done")) {
+				break; // exit from the loop 
+			}
+			newStudent.enrollcource(courseName1);
+			
+		}
+		newStudent.printStudentinfo();
+		
+		
+		
+		
+		
+	}
+
+	public static Student findStudentbyId(String studentId) {
+		
+		Student result = null;     //explicit initialization
+		try {
+		result= studentList
+		.stream().filter(x -> x.getStudentID()
+		.equalsIgnoreCase(studentId))
+        .findFirst()
+        .orElseThrow(()-> new RuntimeException("No Data found !!!"));
+		
+		}
+		catch (RuntimeException e) {
+			System.err.println("Student with ID "+studentId+"not found!!");
+		}
+		
+		return result;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
